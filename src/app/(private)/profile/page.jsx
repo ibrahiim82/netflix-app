b3fe74611e16e260752d65d1467a30c8984d69a1
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-10-31 20:19:34 +0300

    lastcommit

--- src/app/(private)/profile/page.jsx
+++ src/app/(private)/profile/page.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import CardContainer from "./components/CardContainer";
 export const metadata = {
   title: "Profile Page",
 };
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="flex items-center justify-center pt-20">
+      <div className="flex flex-col">
+        <h1 className="text-3xl md:text-6xl text-white text-center">
+          Who's watching?
+        </h1>
+        <CardContainer />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
