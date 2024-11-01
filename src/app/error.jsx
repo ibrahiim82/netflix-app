38722473384018b5d9251f7e6c7486aa8212e506
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-11-01 21:27:03 +0300

    lastcommit

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -10,7 +10,7 @@ export default function Error({ error, reset }) {
 
   return (
     <div className="mt-64 text-center">
-      <h1 className="text-red-600 text-2xl">Something went wrong!</h1>
+      <h1 className="text-red-600 text-2xl">Something went wrong</h1>
     </div>
   );
 }
